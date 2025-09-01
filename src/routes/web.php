f727f4f0425eddb160903fc929a3cfdb6cdb2f98
ishikawa <isikawa_m@outlook.jp>
2025-09-01 21:06:13 +0900

    Remove secret token

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-/*
-　　パーソナルアクセストークンメモにある
-　　⭐git pushで必要です。
-*/
+
 
 Route::get('/register', [UserController::class, 'showRegisterForm']);
