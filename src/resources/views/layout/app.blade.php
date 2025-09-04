cb9a3461d515f34c6c707949c4ed38fbc5127649
ishikawa <isikawa_m@outlook.jp>
2025-09-04 20:16:08 +0900

    header部分のCSS整える

--- src/resources/views/layout/app.blade.php
+++ src/resources/views/layout/app.blade.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>勤怠管理アプリ</title>
   <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/common.css')}}">
+  <link rel="stylesheet" href="{{ asset('css/app.css')}}">
   @yield('css')
 </head>
 
